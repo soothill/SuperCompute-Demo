4c8573133aa19ecb6697d966c211641671382bfa
soothill <darren@soothill.com>
2023-04-20 06:44:18 +0100

    Modified Docker command to use external images

--- redeploy.ps1
+++ redeploy.ps1
@@ -3,4 +3,4 @@ wsl docker rm croit
 wsl docker rm croit-data
 
 wsl docker create --name croit-data croit/croit:latest
-wsl docker run --cap-add=SYS_TIME -v /var/run/docker.sock:/var/run/docker.sock --net=host --restart=always --volumes-from croit-data --name croit -d croit/croit:latest
+wsl docker run --cap-add=SYS_TIME -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/c/images:/images --net=host --restart=always --volumes-from croit-data --name croit -d croit/croit:latest
